a016851a8ac40a38c145378308acf77b6f8272e0
OlegN5 <6370736@gmail.com>
2021-02-17 20:53:19 +0300

    123

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-//import './styles/main.css'
+$el = document.querySelector('#editor').textContent
+//const text= $el.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+//const text= 
+$el = $el.replace(/[.,!?():;]/g,"")
+$el = $el.replace('-',"=")
 
-$el = document.querySelector('#content')
+//const text1 = text.replace(/\s{2,}/g," ")
+a = 'у,е,э,о,а,ы,я,и,ю,У,Е,Э,О,А,Ы,Я,И,Ю'.split(',')
+// text = a.toarray.map(item => {
+    
 
+//     return $el = star($el, i);
+// })
+ 
+//
+//debugger
+console.log(a)
+for (i of a) {
+    $el = star($el, i);
+}
+
+$el = $el.replace("-"," ")
+
+
+function star(string, token) {
+    return string.split(token).join(token+'-');
+  }
 console.log($el)
-console.log($el)
+//debugger
+
